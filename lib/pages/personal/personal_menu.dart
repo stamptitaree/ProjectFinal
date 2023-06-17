2b0456b2980fa3546f2a44698e0badf224f910c2
oomsinboy <oomsinboyyyy@gmail.com>
2023-06-17 22:13:02 +0700

    singleChild

--- lib/pages/personal/personal_menu.dart
+++ lib/pages/personal/personal_menu.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:mytest/widget/appbar_main.dart';
 import 'package:mytest/widget/drawer_main.dart';
-
 
 class PersonalMenu extends StatefulWidget {
   const PersonalMenu({super.key});
@@ -13,24 +14,179 @@ class PersonalMenu extends StatefulWidget {
 class _PersonalMenuState extends State<PersonalMenu> {
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       appBar: const AppbarMain(),
       body: SingleChildScrollView(
         child: SafeArea(
             child: Container(
           width: double.infinity,
           padding: const EdgeInsets.all(15.0),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text('Personal')
-             ],
+              CircleAvatar(
+                radius: 72,
+                backgroundColor: Colors.blueGrey,
+                // backgroundImage: NetworkImage(
+                //     'https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5nRRVgnzYSSwUoPM7rigVHaj4QhdURLfyt90hBPNzf89n8vZ5bp.jpg'),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(fontSize: 18,),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          readOnly: true,
+                          enabled: false,
+                          decoration: InputDecoration(
+                            hintText: 'oomsinboyyyy@gmail.com',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'Password',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          enabled: false,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(color: const Color.fromARGB(66, 0, 0, 0),),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'ชื่อ',
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          enabled: false,
+                          decoration: InputDecoration(
+                            hintText: 'สุดหล่อ',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'นามสกุล',
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          enabled: false,
+                          decoration: InputDecoration(
+                            hintText: 'สมมุติ',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'ประวัติแพ้ยา',
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          enabled: false,
+                          decoration: InputDecoration(
+                            hintText: '-',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Text(
+                      'โรคประจำตัว',
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextFormField(
+                          enabled: false, 
+                          decoration: InputDecoration(
+                            hintText: 'ความดันโลหิต',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide:
+                                      BorderSide(color: Colors.black38))),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         )),
       ),
       drawer: const DrawerMain(),
-
-    
     );
   }
 }
