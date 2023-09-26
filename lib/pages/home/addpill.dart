192cfb7635bfa889e65443f00c61aa577aa9fc34
oomsinboy <oomsinboyyyy@gmail.com>
2023-09-27 01:32:59 +0700

    fix button

--- lib/pages/home/addpill.dart
+++ lib/pages/home/addpill.dart
@@ -506,30 +506,30 @@ class _AddpillState extends State<Addpill> {
                           fontFamily: 'Prompt')),
                 ),
               ),
-              InkWell(
-                onTap: () {
-                  LocalNotification().pendingNotificationRequests();
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 55,
-                  decoration: BoxDecoration(
-                    color: GlobalColors.mainColor,
-                    borderRadius: BorderRadius.circular(6),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: const Text('yyyyy',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'Prompt')),
-                ),
-              )
+              // InkWell(
+              //   onTap: () {
+              //     LocalNotification().pendingNotificationRequests();
+              //   },
+              //   child: Container(
+              //     alignment: Alignment.center,
+              //     height: 55,
+              //     decoration: BoxDecoration(
+              //       color: GlobalColors.mainColor,
+              //       borderRadius: BorderRadius.circular(6),
+              //       boxShadow: [
+              //         BoxShadow(
+              //           color: Colors.black.withOpacity(0.1),
+              //           blurRadius: 10,
+              //         ),
+              //       ],
+              //     ),
+              //     child: const Text('yyyyy',
+              //         style: TextStyle(
+              //             color: Colors.white,
+              //             fontWeight: FontWeight.w600,
+              //             fontFamily: 'Prompt')),
+              //   ),
+              // )
             ],
           ),
         ),
